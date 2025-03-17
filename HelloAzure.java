0fdc2f09afb510c315eb5090eb3354ba6b1478d1
DODDAKA NAVEEN KUMAR <65757392+DODDAKA@users.noreply.github.com>
2025-03-17 15:46:39 +0530

    Create HelloAzure.java

---
+++ HelloAzure.java
@@ -0,0 +1,29 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloAzure extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Hello, Azure!</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Hello, Azure!</h1>");
+            out.println("<p>Welcome to your Java web app deployed on Azure.</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+}
